f31b684f365f278ae563f7dab2ba56a9b3f19bf5
Sahidev1 <theoddrogue@gmail.com>
2022-12-02 14:56:18 +0100

    minor changes

---
+++ LAB2/README.txt
@@ -0,0 +1,3 @@
+The build executable only works for Linux. 
+For other OS will need to compile each java file manually, sorry.
+
--- LAB2/build.sh
+++ LAB2/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -e
+
 FILENAME="temp0q209i013j091z.txt"
 find -name "*.java" > $FILENAME
 javac @"$FILENAME"
 rm $FILENAME
+
+echo "Build successfull!"
+
+echo "Usage: java Main [HOST IP]  [HOST PORT]"
+
 
